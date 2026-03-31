30852a8f68f8f8b4ddcdeea389ec7605dbf94498
Miss Lacoste <makarrroninka@yandex.ru>
2026-03-31 17:36:36 +0300

    added Enigma.hpp file

--- src/Enigma.hpp
+++ src/Enigma.hpp
@@ -1,6 +1,28 @@
-class RotorConfigurationError : public std::runtime_error {
+#ifndef ENIGMA_HPP
+#define ENIGMA_HPP
+
+#include <array>
+#include <string>
+#include "Plugboard.hpp"
+#include "Reflector.hpp"
+#include "Rotor.hpp"
+
+class Enigma
+{
+private:
+    void advanceRotors(std::array<Rotor, 3>& rotors, int& MovingsCount);
+    void applyRingSettings(std::array<Rotor, 3>& rotors);
+    void forwardPassThroughRotors(std::array<Rotor, 3>& rotors, char& character);
+    void reversePassThroughRotors(std::array<Rotor, 3>& rotors, char& character);
 public:
-    explicit RotorConfigurationError(const std::string& message)
-        : std::runtime_error(message) {}
+    std::string ringSettings[3];
+    bool defaultSettings();
+    bool searchCopies(const std::array<int, 3>&);
+    int char_to_int(char);
+    bool input(int);
+    bool Rings(std::string &);
+    void encipher(std::array<Rotor, 3> &, Reflector &, Plugboard &, char &, int &);
+    int start();
 };
-//to be continued
+
+#endif
