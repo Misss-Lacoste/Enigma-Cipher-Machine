92335b635062b05d14716dffa4b840d0b344023f
Miss Lacoste <makarrroninka@yandex.ru>
2026-04-06 14:42:37 +0300

    updatings in Rotor.hpp

--- src/Rotor.hpp
+++ src/Rotor.hpp
@@ -1,18 +1,42 @@
 #ifndef ROTOR_HPP
 #define ROTOR_HPP
+//#include <iostream>
+#include <string>
+#include <string_view>
+#include <array>
+
+//Initial alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//https://www.codesandciphers.org.uk/enigma/index.htm
+//https://www.codesandciphers.org.uk/enigma/rotorspec.htm
+
+constexpr std::array<std::string_view, 5> rotors = {
+    "EKMFLGDQVZNTOWYHXUSPAIBRCJ",  //official configurations
+    "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+    "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+    "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+    "VZBRGITYUPSDNHLXAWMJQOFECK"  
+};
+
+constexpr std::array<std::string_view, 5> inversedRotors = {
+    "UWYGADFPVZBECKMTHXSLRINQOJ", //also official configurations
+    "AJPCZWRLFBDKOTYUQGENHXMIVS",
+    "TAGBPCSDQEUFVNZHYIXJWLRKOM",
+    "HZWVARTNLGUPXQCEJMBSKDYOIF",
+    "QCYLXWENFTZOSMVJUDKGIARPHB"
+};
 
 class Rotor {
 private:
-    int currentPosition; //current position(0–25)
-    int rotorType;       //rotor type(1–5)
-    char ring;           //ring setting
+    std::string rotor;
+    std::string inversedRotor;
 
 public:
-    bool isInTurnoverPosition() const;
+    std::string Alphabet =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string inversedAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char ring;
+    void ConfigureRotor(int);
+    void substitute(char &, char);
     void offset();
-    void ringApply(char ringSetting);
-    void substitute(char& ch, char direction);
-    void setRotor(int type);
+    void RingConfig(char);
 };
-
 #endif
