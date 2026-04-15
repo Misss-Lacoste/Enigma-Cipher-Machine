191e13a4a06c01d7341f8798f89959c7c7e2f8b9
Miss Lacoste <makarrroninka@yandex.ru>
2026-04-15 16:19:29 +0300

    upd main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <fstream>
 
 void showEnigmaInfo() {
-    std::ifstream file("enigma_info.txt");
+    std::ifstream file("enigma_info_eng.txt");
     std::string line;
 
     while (std::getline(file, line)) {
@@ -15,39 +15,49 @@ void showEnigmaInfo() {
     }
 	file.close();
 
-    std::cout << "Press 'Enter' to continue.";
-    std::cin.ignore();
+    std::cout << "Press 'Enter' to go back to the main menu...";
+    //std::cin.ignore();
     std::cin.get();
 }
 
 void showAttackMethods() {
-	std::ifstream file("attack_methods.txt");
-    if (!file.is_open()) {
-        std::cout << "Unavailable now.\n";
-        return;
-    }
-
+	std::ifstream file("crib_attack.txt");
     std::string line;
+
     while (std::getline(file, line)) {
         std::cout << line << "\n";
     }
     file.close();
 
-    std::cout << "Press 'Enter' to continue.";
-    std::cin.ignore();
+    std::cout << "Press 'Enter' to go back to the main menu...";
+    //std::cin.ignore();
     std::cin.get();
 }
 
-void Help() {}
+void Help() {
+	std::ifstream file("help.txt");
+	std::string line;
+
+	while (std::getline(file, line)) {
+        std::cout << line << "\n";
+    }
+	file.close();
+
+    std::cout << "Press 'Enter' to go back to the main menu...";
+    //std::cin.ignore();
+    std::cin.get();
+}
 
 void showMenu() {
-	std::cout << "*Enigma Options*\n";
+	std::cout << "\n*********************************************\n";
+	std::cout << "Enigma Options.\n";                           
 	std::cout << "1. Encrypt the message.\n";
 	std::cout << "2. Decrypt the message.\n";
 	std::cout << "3. Look at the Enigma Historical Information.\n";
 	std::cout << "4. Study attack methods.\n";
 	std::cout << "5. Help.\n";
 	std::cout << "6. Exit.\n";
+	std::cout << "*********************************************\n";
 }
 
 void amazingEffect(const std::string &text, int sleepingTime = 50) {
@@ -79,15 +89,15 @@ int main() {
 			do {
 				enigma.start();
 				std::cout << "To continue encrypting press 'Y', to stop press 'N': ";
-				std::cin >> UserInput;
-				//std::cin.ignore();
+				std::cin >> UserInput; //
+				std::cin.ignore();
 				std::cout << std::endl;
 			} while (UserInput == 'Y' || UserInput == 'y');
 				break;
 		}
 		case 2: {
 			std::cout << "Decryption is still coding.\n";
-			std::cout << "Press 'Enter' to continue.";
+			std::cout << "Press 'Enter' to go back to the main menu...";
 			std::cin.get();
 			break;
 		}
@@ -101,13 +111,14 @@ int main() {
 		}
 		case 5: {
 			Help();
+			break;
 		}
 		case 6: {
 			amazingEffect("See you later!");
 			break;
 		}
-		deafult: {
-			std::cout << "Invalid option. Try again.\n";
+		default: {
+			std::cout << "Unfortunately, you misspelled. Enter option point again.\n";
 			break;
 		}
 	}
