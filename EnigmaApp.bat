e9227db5c5a7612a80591591b3e1387036a71220
Miss Lacoste <makarrroninka@yandex.ru>
2026-05-06 19:29:39 +0300

    added .bat file

---
+++ EnigmaApp.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d "%~dp0"
+
+if not exist "backend\enigma_app.exe" (
+    echo Error: Backend executable not found! Please run 'mingw32-make' first.
+    pause
+    exit /b
+)
+
+if exist "enigma_env\Scripts\pythonw.exe" (
+    start "" "enigma_env\Scripts\pythonw.exe" "frontend\main.py"
+) else (
+    start "" "pythonw.exe" "frontend\main.py"
+)
